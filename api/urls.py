ef28da54e8a5a4b27fff91c177ff782218a82c47
jamesmarcooo <reyes.jamesmarco03@gmail.com>
2025-06-27 15:05:07 +0800

    BSBD-7.2 - Apply v2 api

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("v1/", include("api.v1.urls"))
+    path("v1/", include("api.v1.urls")),
+    path("v2/", include("api.v2.urls")),
 ]
--- api/v2/urls.py
+++ api/v2/urls.py
@@ -1,12 +1,7 @@
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
 from rest_framework.routers import DefaultRouter
 
-from api.v1.views import CommentCreateAPIView, PostViewSet
+from api.v2.views import CommentCreateAPIView, PostViewSet
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="post")
@@ -18,4 +13,5 @@ urlpatterns = [
         CommentCreateAPIView.as_view(),
         name="post-comment-create",
     ),
+
 ]
